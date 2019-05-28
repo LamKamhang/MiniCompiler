4b7cc6ab21149cbf62ae3a5a329fb29a253f2d83
LamKamhang <Cool_Lam@outlook.com>
2019-05-28 23:25:08 +0800

    build leaf node

--- src/main.cxx
+++ src/main.cxx
@@ -6,26 +6,27 @@ extern FILE* yyout;
 
 int main (int argc, char **argv)
 {
-    if (argc >= 2)
-    {
-        if ((yyin = fopen(argv[1], "r")) == NULL)
-        {
-            fprintf(stderr, "Cannot open file %s\n", argv[1]);
-            return 1;
-        }
-        if (argc >= 3)
-        {
-            yyout = fopen(argv[2], "w");
-        }
-    }
+	if (argc >= 2)
+	{
+		if ((yyin = fopen(argv[1], "r")) == NULL)
+		{
+			fprintf(stderr, "Cannot open file %s\n", argv[1]);
+			return 1;
+		}
+		if (argc >= 3)
+		{
+			yyout = fopen(argv[2], "w");
+		}
+	}
 
 	yyparse();
 
-    if (argc >= 2)
-    {
-	  fclose(yyin);
-	  if (argc >= 3) fclose(yyout);
+	if (argc >= 2)
+	{
+		fclose(yyin);
+		if (argc >= 3) 
+			fclose(yyout);
 	}
 
-    return 0;
+	return 0;
 }
