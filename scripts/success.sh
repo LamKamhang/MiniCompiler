51613b2e9a5766fec4889408843458ccf587557f
LamKamhang <Cool_Lam@outlook.com>
2019-05-29 00:02:46 +0800

    update success script

--- scripts/success.sh
+++ scripts/success.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+function test(){
+	for element in `ls $1`
+	do
+		dir_or_file=$1"/"$element
+		if [ -d $dir_or_file ]; then
+			test $dir_or_file
+		else
+			if [[ `./build/release/ncc $dir_or_file` != "ok" ]]; then
+				exit 1
+			fi
+		fi
+	done
+}
+
+test_dir="test"
+
+test $test_dir
+echo "pass parser test"
