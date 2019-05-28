d5e898e1f4a620d9fa931c6acab6a3c1dce08213
LamKamhang <Cool_Lam@outlook.com>
2019-05-28 23:38:41 +0800

    remove json folder to ast and remove include cc bug

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -2,20 +2,29 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include "include/json/jsoncpp.cc"
+#include "include/json/include/json.h"
+#include "include/json/include/json-forwards.h"
 
 namespace ast
 {
 class Node
 {
 public:
-    std::string type;
-    int pos[4] = {0};
-    std::vector<std::shared_ptr<Node>> children;
-    std::string value; // only used for a few non-terminals
-    Node() = default;
-    Node(const std::string &type) : type(type){};
+	Node() = default;
+	Node(const std::string &type) : type(type){};
+	Node(const std::string &type, const std::string &value, int x1, int y1, int x2, int y2)
+		: type(type)
+		//, pos({x1, y1, x2, y2})
+		, value(value)
+	{pos[0] = x1; pos[1] = y1; pos[2] = x2; pos[3] = y2;}
+
+public:
+	std::string type;
+	int pos[4] = {0};
+	std::vector<std::shared_ptr<Node>> children;
+	std::string value; // only used for a few non-terminals
 };
+
 std::shared_ptr<ast::Node> imports(Json::Value &json);
 Json::Value exports(std::shared_ptr<ast::Node> node);
 } // namespace ast
