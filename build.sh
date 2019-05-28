74a3aa1cae656d14cfb1a3702df8f3a65eab3643
LamKamhang <Cool_Lam@outlook.com>
2019-05-28 23:24:36 +0800

    remove flex and bison from cmake and call them in the shell script

--- build.sh
+++ build.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	cmake . -Bbuild/debug -DCMAKE_BUILD_TYPE=Debug
-	if [ $? -eq 0 ]; then
-		cd build/debug
-		make
-	fi
-elif [ $# -eq 1 ]; then
-    if [ $1 == "profile" ]; then
-	cmake . -Bbuild/debug -DCMAKE_BUILD_TYPE=Debug -DPROFILE_FLAG=1
-	if [ $? -eq 0 ]; then
-	    cd build/debug
-	    make
-	fi
-    elif [ $1 == "release" ]; then
-	cmake . -Bbuild/release -DCMAKE_BUILD_TYPE=Release
-	if [ $? -eq 0 ]; then
-	    cd build/release
-	    make
-	fi
-    else
-	echo "unknown argument"
-    fi
-elif [ $# -eq 2 ]; then
-    if [ $1 == "release" ]; then
-	if [ $2 == "profile" ]; then
-	    cmake . -Bbuild/release -DCMAKE_BUILD_TYPE=Release -DPROFILE_FLAG=1
-	    if [ $? -eq 0 ]; then
-		cd build/release
-		make
-	    fi
-	else
-	    echo "unknown argument"
-	fi
-    else
-	echo "unknown argument"
-    fi
+	folder="build/debug"
+	flag="-DCMAKE_BUILD_TYPE=Debug"
+elif [ $# -eq 1 ] && [ $1 == "profile" ]; then
+	folder="build/debug"
+	flag="-DCMAKE_BUILD_TYPE=Debug -DPROFILE_FLAG=1"
+elif [ $# -eq 1 ] && [ $1 == "release" ]; then
+	folder="build/release"
+	flag="-DCMAKE_BUILD_TYPE=Release"
+elif [ $# -eq 2 ] && [ $1 == "release" ] && [ $2 == "profile" ]; then
+	folder="build/release"
+	flag="-DCMAKE_BUILD_TYPE=Release -DPROFILE_FLAG=1"
 else
-	echo "build failed!"
+	echo "unvalid arguments"
+	exit 1
+fi
+
+pre_build(){
+	echo "ready to build yacc and lex file"
+	cd src
+	bison -d parser.ypp -o parser.cpp
+	if [ $? -ne 0 ]; then
+		echo "yacc generate failed."
+		exit 1
+	else
+		flex -o scanner.cpp scanner.lex
+		if [ $? -ne 0 ]; then
+			echo "flex generate failed."
+			exit 1
+		fi
+	fi
+	echo "pre build finished."
+	cd ..
+}
+
+pre_build
+
+cmake . -B$folder $flag
+if [ $? -ne 0 ]; then
+	echo "cmake failed."
+	exit 1
+fi
+cd $folder
+make
+if [ $? -ne 0 ]; then
+	echo "make failed."
+	exit 1
 fi
