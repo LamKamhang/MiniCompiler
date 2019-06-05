4c349af00732825cf3f50d21f221af638f42c9af
lincandong <571611998@qq.com>
2019-06-05 17:45:42 +0800

    merge

--- test/ir/main.cc
+++ test/ir/main.cc
@@ -10,7 +10,68 @@
 #include <fstream>
 
 extern std::string cwd;
-std::unique_ptr<llvm::Module> TheModule;
+
+int tar_generate()
+{
+
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+
+    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+    Module->setTargetTriple(TargetTriple);
+
+    std::string Error;
+    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+
+    // Print an error and exit if we couldn't find the requested target.
+    // This generally occurs if we've forgotten to initialise the
+    // TargetRegistry or we have a bogus target triple.
+    if (!Target)
+    {
+        llvm::errs() << Error;
+        return 1;
+    }
+
+    auto CPU = "generic";
+    auto Features = "";
+
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
+    auto TheTargetMachine =
+        Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+    Module->setDataLayout(TheTargetMachine->createDataLayout());
+
+    auto Filename = "output.o";
+    std::error_code EC;
+    llvm::raw_fd_ostream dest(Filename, EC, llvm::sys::fs::F_None);
+
+    if (EC)
+    {
+        llvm::errs() << "Could not open file: " << EC.message();
+        return 1;
+    }
+
+    llvm::legacy::PassManager pass;
+    auto FileType = llvm::TargetMachine::CGFT_ObjectFile;
+
+    if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType))
+    {
+        llvm::errs() << "TheTargetMachine can't emit a file of this type";
+        return 1;
+    }
+
+    pass.run(*Module);
+    dest.flush();
+
+    llvm::outs() << "Wrote " << Filename << "\n";
+
+    return 0;
+}
+
 int main()
 {
     const std::string &path = "test/function_definition/";
@@ -31,69 +92,8 @@ int main()
     file << ir_code;
     file.close();
 
-    auto TheModule=std::move(Module);
     tar_generate();
-    
 }
 //tutorial demo run instruction
 //clang++ -g -O3 toy.cpp `llvm-config --cxxflags --ldflags --system-libs --libs all` -o toy
 //clang++ main.cpp output.o -o main
-int tar_generate()
-    {
-        llvm::raw_ostream &errs();
-        llvm::raw_ostream &outs();
-
-        llvm::InitializeAllTargetInfos();
-        llvm::InitializeAllTargets();
-        llvm::InitializeAllTargetMCs();
-        llvm::InitializeAllAsmParsers();
-        llvm::InitializeAllAsmPrinters();
-
-        auto TargetTriple = llvm::sys::getDefaultTargetTriple();
-        TheModule->setTargetTriple(TargetTriple);
-
-        std::string Error;
-        auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
-
-        // Print an error and exit if we couldn't find the requested target.
-        // This generally occurs if we've forgotten to initialise the
-        // TargetRegistry or we have a bogus target triple.
-        if (!Target) {
-            errs() << Error;
-            return 1;
-        }
-
-        auto CPU = "generic";
-        auto Features = "";
-
-        llvm::TargetOptions opt;
-        auto RM = llvm::Optional<llvm::Reloc::Model>();
-        auto TheTargetMachine =
-            Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
-
-        TheModule->setDataLayout(TheTargetMachine->createDataLayout());
-
-        auto Filename = "output.o";
-        std::error_code EC;
-        llvm::raw_fd_ostream dest(Filename, EC, llvm::sys::fs::F_None);
-
-        if (EC) {
-            errs() << "Could not open file: " << EC.message();
-            return 1;
-        }
-
-        llvm::legacy::PassManager pass;
-        auto FileType = llvm::TargetMachine::CGFT_ObjectFile;
-
-        if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
-            errs() << "TheTargetMachine can't emit a file of this type";
-            return 1;
-        }
-
-        pass.run(*TheModule);
-        dest.flush();
-
-        outs() << "Wrote " << Filename << "\n";
-
-        return 0;
-    }
