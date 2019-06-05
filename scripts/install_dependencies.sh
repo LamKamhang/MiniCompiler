3f1f8b91a0eff4fa95f25dfa5ee7cade55892ec2
LamKamhang <Cool_Lam@outlook.com>
2019-06-05 14:45:22 +0800

    remove boost lib dev in cmake

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install -y g++ cmake
-sudo apt-get install -y llibc++-dev libc++abi-dev
+sudo apt-get install -y llibc++-dev libc++abi-dev llvm-dev
 sudo apt-get install -y bison flex
