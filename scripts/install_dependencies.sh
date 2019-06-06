af460ae915d5babde7acb5d06e0a18553352d612
LamKamhang <Cool_Lam@outlook.com>
2019-06-06 10:34:47 +0800

    update dependency

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install -y g++ cmake
-sudo apt-get install -y llibc++-dev libc++abi-dev llvm-dev
+sudo apt-get install -y libc++-dev libc++abi-dev llvm-dev llvm-runtime llvm
 sudo apt-get install -y bison flex
